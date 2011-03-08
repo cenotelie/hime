4b73bce91c702d922389da584d8a8ca4f3c9ae58
Laurent Wouters <lwouters@cenotelie.fr>
2011-03-08 18:13:30 +0000

    Modified himecc options

--- himecc/Options.cs
+++ himecc/Options.cs
@@ -5,35 +5,31 @@ using CommandLine;
 
 namespace Hime.HimeCC
 {
-    class Options
+    public class Options
     {
         public Options()
         {
-            Inputs = new string[] { };
-            GrammarName = "none";
-            Namespace = "";
-            Method = Parsers.ParsingMethod.LALR1;
-            LexerFile = null;
-            ParserFile = "none.cs";
-            ExportHTMLLog = true;
+            Inputs = new List<string>();
+            Method = Parsers.ParsingMethod.RNGLALR1;
+            ExportHTMLLog = false;
         }
 
-        [OptionArray("i", "input", Required=true, HelpText="Input grammar files")]
-        public string[] Inputs;
+        [ValueList(typeof(List<string>))]
+        public List<string> Inputs;
 
-        [Option("g", "grammar", Required=true, HelpText="Name of the grammar for which a parser shall be generated")]
+        [Option("g", "grammar", Required=false, HelpText="Name of the grammar for which a parser shall be generated")]
         public string GrammarName;
 
-        [Option("n", "namespace", Required = true, HelpText = "Namespace for the generated Lexer and Parser classes")]
+        [Option("n", "namespace", Required = false, HelpText = "Namespace for the generated Lexer and Parser classes")]
         public string Namespace;
 
-        [Option("m", "method", Required = true, HelpText = "Name of the parsing method to use: LR0|LR1|LALR1|RNGLR1|RNGLALR1")]
+        [Option("m", "method", Required = false, HelpText = "Name of the parsing method to use: LR0|LR1|LALR1|RNGLR1|RNGLALR1")]
         public Parsers.ParsingMethod Method;
 
         [Option(null, "lexer", Required = false, HelpText = "Path and name of the file for the generated lexer")]
         public string LexerFile;
 
-        [Option(null, "parser", Required = true, HelpText = "Path and name of the file for the generated parser")]
+        [Option(null, "parser", Required = false, HelpText = "Path and name of the file for the generated parser")]
         public string ParserFile;
 
         [Option("l", "log", Required = false, HelpText = "True to export the generation log (HTML file)")]
@@ -47,9 +43,21 @@ namespace Hime.HimeCC
             help.AdditionalNewLineAfterOption = true;
             help.AddPreOptionsLine("This is free software. You may redistribute copies of it under the terms of");
             help.AddPreOptionsLine("the LGPL License <http://www.gnu.org/licenses/lgpl.html>.");
-            help.AddPreOptionsLine("Usage: himecc -i MyGram.gram -g MyGrammar -n Analyser -m LALR1 --parser MyGram.cs");
+            help.AddPreOptionsLine("Usage: himecc MyGram.gram");
+            help.AddPreOptionsLine("Usage: himecc MyGram.gram -g MyGrammar -n Analyser -m LALR1 --parser MyGram.cs");
             help.AddOptions(this);
             return help;
+        }
+
+        public bool Check()
+        {
+            if (Inputs.Count == 0)
+                return false;
+            if (Namespace == null)
+                Namespace = System.IO.Path.GetFileNameWithoutExtension(Inputs[0]);
+            if (ParserFile == null)
+                ParserFile = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Inputs[0]), System.IO.Path.GetFileNameWithoutExtension(Inputs[0]) + ".cs");
+            return true;
         }
     }
 }
--- himecc/Program.cs
+++ himecc/Program.cs
@@ -4,21 +4,33 @@ using System.Text;
 
 namespace Hime.HimeCC
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
+        {
+            Options options = ParseArguments(args);
+            if (options != null)
+                Execute(options);
+            else
+                System.Console.WriteLine((new Options()).GetUsage());
+        }
+
+        public static Options ParseArguments(string[] args)
         {
             Options options = new Options();
             CommandLine.ICommandLineParser parser = new CommandLine.CommandLineParser();
             if (parser.ParseArguments(args, options))
-                Execute(options);
-            else
-                System.Console.WriteLine(options.GetUsage());
+            {
+                if (!options.Check())
+                    return null;
+                return options;
+            }
+            return null;
         }
 
-        private static void Execute(Options options)
+        public static void Execute(Options options)
         {
-            Hime.Parsers.CompilationTask Task = new Parsers.CompilationTask(options.Inputs, options.GrammarName, options.Method, options.Namespace, options.LexerFile, options.ParserFile, options.ExportHTMLLog, false);
+            Hime.Parsers.CompilationTask Task = new Parsers.CompilationTask(options.Inputs.ToArray(), options.GrammarName, options.Method, options.Namespace, options.LexerFile, options.ParserFile, options.ExportHTMLLog, false);
             Task.Execute();
         }
     }
