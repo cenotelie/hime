dabee3eb868139471a993eda7881a8af0ff2ea81
Laurent Wouters <lwouters@cenotelie.fr>
2011-03-09 20:45:51 +0000

    new test

--- Hime.VSTests/himecc/Suite01_Compile.cs
+++ Hime.VSTests/himecc/Suite01_Compile.cs
@@ -10,20 +10,22 @@ namespace Hime.VSTests.himecc
     public class Suite01_Compile
     {
         private const string p_Directory = "Test";
-        private const string p_TempGrammar = p_Directory + "\\MathExp.gram";
-        private const string p_TempFile = p_Directory + "\\MathExp.cs";
 
-        private System.Reflection.Assembly Compile(string[] command)
+        private void Generate(string[] command)
         {
             if (System.IO.Directory.Exists(p_Directory))
                 System.IO.Directory.Delete(p_Directory, true);
             System.IO.Directory.CreateDirectory(p_Directory);
-            Integration.Tools.Export("MathExp.gram", p_TempGrammar);
-            string redist = System.Reflection.Assembly.GetAssembly(typeof(Redist.Parsers.ILexer)).Location;
-            System.IO.File.Copy(redist, p_Directory + "\\Hime.Redist.dll");
+            Integration.Tools.Export(System.IO.Path.GetFileName(command[0]), command[0]);
             HimeCC.Options options = HimeCC.Program.ParseArguments(command);
             HimeCC.Program.Execute(options);
-            string code = System.IO.File.ReadAllText(p_TempFile);
+        }
+        private System.Reflection.Assembly Compile(string[] command)
+        {
+            Generate(command);
+            string redist = System.Reflection.Assembly.GetAssembly(typeof(Redist.Parsers.ILexer)).Location;
+            System.IO.File.Copy(redist, p_Directory + "\\Hime.Redist.dll");
+            string code = System.IO.File.ReadAllText(command[0].Replace(".gram", ".cs"));
             System.CodeDom.Compiler.CodeDomProvider compiler = System.CodeDom.Compiler.CodeDomProvider.CreateProvider("C#");
             System.CodeDom.Compiler.CompilerParameters compilerparams = new System.CodeDom.Compiler.CompilerParameters();
             compilerparams.GenerateExecutable = false;
@@ -38,17 +40,23 @@ namespace Hime.VSTests.himecc
         [TestMethod]
         public void Test001_DefaultNamespace_GeneratedLexer()
         {
-            System.Reflection.Assembly assembly = Compile(new string[] { p_TempGrammar });
+            System.Reflection.Assembly assembly = Compile(new string[] { p_Directory + "\\MathExp.gram" });
             System.Type lexer = assembly.GetType("MathExp.MathExp_Lexer");
             Assert.IsNotNull(lexer);
         }
 
         [TestMethod]
-        public void Test001_DefaultNamespace_GeneratedParser()
+        public void Test002_DefaultNamespace_GeneratedParser()
         {
-            System.Reflection.Assembly assembly = Compile(new string[] { p_TempGrammar });
+            System.Reflection.Assembly assembly = Compile(new string[] { p_Directory + "\\MathExp.gram" });
             System.Type lexer = assembly.GetType("MathExp.MathExp_Parser");
             Assert.IsNotNull(lexer);
+        }
+
+        [TestMethod]
+        public void Test003_DontCrashOnEmptyFile()
+        {
+            Generate(new string[] { p_Directory + "\\Empty.gram" });
         }
     }
 }
