7719afa633ef19aaba6eb2c7e35c59eb6e01cf77
Charles Hymans <charles.hymans@wanadoo.fr>
2011-07-17 11:57:56 +0000

    [add] More nunit tests.

---
+++ Hime.NUnit/Integration/Suite00_Ambiguous.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Hime.NUnit.Integration
+{
+    [TestFixture]
+    public class Suite00_Ambiguous
+    {
+        [Test]
+        public void Test001_ReturnsFalseOnConflictuousGrammar_LALR1()
+        {
+            string grammar = "public grammar cf Test { options{ Axiom=\"test\"; } terminals{} rules{ test->a|b; a->'x'; b->'x'; }  }";
+            Assert.IsFalse(Tools.BuildRawText(grammar, Parsers.ParsingMethod.LALR1));
+        }
+
+        [Test]
+        public void Test002_ReturnsFalseOnConflictuousGrammar_LR1()
+        {
+            string grammar = "public grammar cf Test { options{ Axiom=\"test\"; } terminals{} rules{ test->a|b; a->'x'; b->'x'; }  }";
+            Assert.IsFalse(Tools.BuildRawText(grammar, Parsers.ParsingMethod.LR1));
+        }
+
+        [Test]
+        public void Test003_FindsLALR1AmbiguousAndLR1NonAmbiguous()
+        {
+            string grammar = "public grammar cf Test { options{ Axiom=\"S\"; } terminals{} rules{ A->'d'; B->'d'; S->A'a'|'b'A'c'|B'c'|'b'B'a'; } }";
+            Assert.IsFalse(Tools.BuildRawText(grammar, Parsers.ParsingMethod.LALR1));
+            Assert.IsTrue(Tools.BuildRawText(grammar, Parsers.ParsingMethod.LR1));
+        }
+
+        // TODO: this test seems to fail because of a stack overflow!
+        [Ignore]
+        [Test]
+        public void Test004_FindsShiftReduceForLALR1()
+        {
+            string grammar = "public grammar cf Test { options{ Axiom=\"X\"; } terminals{} rules{ X->'a'X | 'a'X 'b'X;} }";
+            Assert.IsFalse(Tools.BuildRawText(grammar, Parsers.ParsingMethod.LALR1));
+        }
+
+        // TODO: this test seems to fail because of a stack overflow!
+        [Ignore]
+        [Test]
+        public void Test005_FindsShiftReduceForLR1()
+        {
+            string grammar = "public grammar cf Test { options{ Axiom=\"X\"; } terminals{} rules{ X->'a'X | 'a'X 'b'X;} }";
+            Assert.IsFalse(Tools.BuildRawText(grammar, Parsers.ParsingMethod.LR1));
+        }
+
+        [Test]
+        public void Test006_FindsAmbigousGrammarLALR1()
+        {
+            Assert.IsFalse(Tools.BuildResource("LALR1-ambiguous.gram", "AmbiguousLALR1", Parsers.ParsingMethod.LALR1));
+        }
+
+        [Test]
+        public void Test007_FindsAmbigousGrammarLR1()
+        {
+            Assert.IsFalse(Tools.BuildResource("LALR1-ambiguous.gram", "AmbiguousLALR1", Parsers.ParsingMethod.LR1));
+        }
+    }
+}
---
+++ Hime.NUnit/Integration/Tools.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hime.NUnit.Integration
+{
+    static class Tools
+    {
+        public static bool BuildRawText(string text, Hime.Parsers.ParsingMethod method)
+        {
+            Parsers.CompilationTask Task = new Parsers.CompilationTask();
+            Task.InputRawData.Add(text);
+            Task.GrammarName = "Test";
+            Task.Method = method;
+            Task.Namespace = "Analyze";
+            Task.ParserFile = "TestAnalyze.cs";
+            Kernel.Reporting.Report Report = Task.Execute();
+            foreach (Kernel.Reporting.Section section in Report.Sections)
+                foreach (Kernel.Reporting.Entry entry in section.Entries)
+                    if (entry.Level == Kernel.Reporting.Level.Error)
+                        return false;
+            return true;
+        }
+        public static bool BuildResource(string file, string name, Hime.Parsers.ParsingMethod method)
+        {
+            Parsers.CompilationTask Task = new Parsers.CompilationTask();
+            Task.InputFiles.Add(file);
+            Task.GrammarName = "Test";
+            Task.Method = method;
+            Task.Namespace = "Analyze";
+            Task.ParserFile = "TestAnalyze.cs"; Kernel.Reporting.Report Report = Task.Execute();
+            foreach (Kernel.Reporting.Section section in Report.Sections)
+                foreach (Kernel.Reporting.Entry entry in section.Entries)
+                    if (entry.Level == Kernel.Reporting.Level.Error)
+                        return false;
+            return true;
+        }
+
+
+        public static void Export(string resourceName, string fileName)
+        {
+            System.Reflection.Assembly p_Assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            string p_DefaultPath = "Resources";
+            Kernel.Resources.ResourceAccessor accessor = new Kernel.Resources.ResourceAccessor(p_Assembly, p_DefaultPath);
+            accessor.Export(resourceName, fileName);
+            accessor.Close();
+        }
+        public static string GetAllTextFor(string Name)
+        {
+            System.Reflection.Assembly p_Assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            string p_DefaultPath = "Resources";
+            Kernel.Resources.ResourceAccessor accessor = new Kernel.Resources.ResourceAccessor(p_Assembly, p_DefaultPath);
+            string data = accessor.GetAllTextFor(Name);
+            accessor.Close();
+            return data;
+        }
+    }
+}
