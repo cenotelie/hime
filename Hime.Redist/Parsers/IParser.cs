acbf35cdb0ec0158e0bf8233e1398dc477d133d0
Laurent Wouters <lwouters@cenotelie.fr>
2011-07-20 17:04:59 +0000

    [clean] Split Parser.cs file in Redist

---
+++ Hime.Redist/Parsers/IParser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Hime.Redist.Parsers
+{
+    /// <summary>
+    /// Represents a parser
+    /// </summary>
+    public interface IParser
+    {
+        /// <summary>
+        /// Gets the errors encountered by the parser
+        /// </summary>
+        ICollection<ParserError> Errors { get; }
+        /// <summary>
+        /// Runs the parser and return the root of the abstract syntax tree
+        /// </summary>
+        /// <returns>The root of the abstract syntax tree representing the input, or null if errors when encountered</returns>
+        SyntaxTreeNode Analyse();
+    }
+}
---
+++ Hime.Redist/Parsers/ParserError.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Hime.Redist.Parsers
+{
+    /// <summary>
+    /// Represents an error in a parser
+    /// </summary>
+    public interface ParserError
+    {
+        /// <summary>
+        /// Gets the error's message
+        /// </summary>
+        string Message { get; }
+    }
+}
