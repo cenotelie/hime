5abea724915d136fc8ced31b439a9b1b75ad57ef
Laurent Wouters <lwouters@cenotelie.fr>
2011-09-14 12:45:58 +0000

    Refactored demo

---
+++ Hime.Demo/Tasks/ANSICParse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hime.Demo.Tasks
+{
+    class ANSICParse : IExecutable
+    {
+        public void Execute()
+        {
+            Analyser.ANSI_C_Lexer lexer = new Analyser.ANSI_C_Lexer(new System.IO.StreamReader("test.c"));
+            Analyser.ANSI_C_Parser parser = new Analyser.ANSI_C_Parser(lexer);
+            Hime.Redist.Parsers.SyntaxTreeNode root = parser.Analyse();
+
+            foreach (Hime.Redist.Parsers.LexerError error in lexer.Errors) System.Console.WriteLine(error.ToString());
+            foreach (Hime.Redist.Parsers.ParserError error in parser.Errors) System.Console.WriteLine(error.ToString());
+            if (root != null)
+            {
+                LangTest.WinTreeView window = new LangTest.WinTreeView(root);
+                window.ShowDialog();
+            }
+        }
+    }
+}
---
+++ Hime.Demo/Tasks/Compile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hime.Parsers;
+
+namespace Hime.Demo.Tasks
+{
+    class Compile : IExecutable
+    {
+        public void Execute()
+        {
+            CompilationTask task = new CompilationTask();
+            task.Namespace = "Analyser";
+            task.ExportLog = true;
+            task.ExportDoc = true;
+            task.ExportVisuals = true;
+            task.InputFiles.Add("Languages\\Test.gram");
+            task.ParserFile = "Test.cs";
+            task.Method = EParsingMethod.LRStar;
+            task.DOTBinary = "C:\\Program Files\\Graphviz 2.28\\bin\\dot.exe";
+            task.Execute();
+        }
+    }
+}
