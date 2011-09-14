c445bccae627b7045899a1641a2ed892d3fa10d7
Laurent Wouters <lwouters@cenotelie.fr>
2011-09-14 13:32:49 +0000

    Auto-implemented properties in CompilationTask

--- Hime.CentralDogma/Hime/Parsers/CompilationTask.cs
+++ Hime.CentralDogma/Hime/Parsers/CompilationTask.cs
@@ -9,130 +9,60 @@ namespace Hime.Parsers
     public sealed class CompilationTask
     {
         // parameters
-        private List<string> rawInputs;
-        private List<string> fileInputs;
-        private string _namespace;
-        private bool exportDebug;
-        private bool exportLog;
-        private bool exportDoc;
-        private bool exportVisuals;
-        private string dotBinary;
-        private bool multithreaded;
-
-        public ICollection<string> InputRawData { get { return rawInputs; } }
-        public ICollection<string> InputFiles { get { return fileInputs; } }
-        
+        public ICollection<string> InputRawData { get; private set; }
+        public ICollection<string> InputFiles { get; private set; }
         // TODO: should never be null
-        public string GrammarName
-        {
-            private get;
-            set;
-        }
+        public string GrammarName { get; set; }
+        public string Namespace { get; set; }
+        public EParsingMethod Method { get; set; }
+        public string LexerFile { get; set; }
+        public string ParserFile { get; set; }
+        public bool ExportDebug { get; set; }
+        public bool ExportLog { get; set; }
+        public bool ExportDoc { get; set; }
+        public bool ExportVisuals { get; set; }
+        public string DOTBinary { get; set; }
+        public bool Multithreaded { get; set; }
+        internal EAccessModifier GeneratedCodeModifier { get; private set; }
         
-        // TODO: should use default getters in the whole solution
-        public string Namespace
-        {
-            get { return _namespace; }
-            set { _namespace = value; }
-        }
-
-        public EParsingMethod Method
-        {
-            private get;
-            set;
-        }
-        
-        public string LexerFile
-        {
-            get;
-            set;
-        }
-        public string ParserFile
-        {
-            get;
-            set;
-        }
-        
-        internal EAccessModifier GeneratedCodeModifier
-        {
-            get;
-            private set;
-        }
-        
-        public bool ExportDebug
-        {
-            get { return exportDebug; }
-            set { exportDebug = value; }
-        }
-        public bool ExportLog
-        {
-            get { return exportLog; }
-            set { exportLog = value; }
-        }
-        public bool ExportDoc
-        {
-            get { return exportDoc; }
-            set { exportDoc = value; }
-        }
-        public bool ExportVisuals
-        {
-            get { return exportVisuals; }
-            set { exportVisuals = value; }
-        }
-        public string DOTBinary
-        {
-            get { return dotBinary; }
-            set { dotBinary = value; }
-        }
-        public bool Multithreaded
-        {
-            get { return multithreaded; }
-            set { multithreaded = value; }
-        }
-
         // internal data
         private Kernel.Namespace root;
-        public Reporter reporter;
-        private ParserGenerator generator;
-        private StreamWriter lexerWriter;
-        private StreamWriter parserWriter;
-        private string docFile;
 
-        internal Kernel.Reporting.Reporter Reporter { get { return reporter; } }
-        internal ParserGenerator ParserGenerator { get { return generator; } }
-        internal System.IO.StreamWriter LexerWriter { get { return lexerWriter; } }
-        internal System.IO.StreamWriter ParserWriter { get { return parserWriter; } }
-        internal string Documentation { get { return docFile; } }
-        
-        public Report Result { get { return this.reporter.Result; } }
+        internal Kernel.Reporting.Reporter Reporter { get; private set; }
+        internal ParserGenerator ParserGenerator { get; private set; }
+        internal System.IO.StreamWriter LexerWriter { get; private set; }
+        internal System.IO.StreamWriter ParserWriter { get; private set; }
+        internal string Documentation { get; private set; }
+
+        public Report Result { get { return Reporter.Result; } }
 
         public CompilationTask()
         {
-            rawInputs = new List<string>();
-            fileInputs = new List<string>();
-            this.GeneratedCodeModifier = EAccessModifier.Public;
-            this.Method = EParsingMethod.RNGLALR1;
-            exportDebug = false;
-            exportLog = false;
-            exportDoc = false;
-            exportVisuals = false;
-            multithreaded = true;
-            this.reporter = new Reporter();
+            InputRawData = new List<string>();
+            InputFiles = new List<string>();
+            Method = EParsingMethod.RNGLALR1;
+            ExportDebug = false;
+            ExportLog = false;
+            ExportDoc = false;
+            ExportVisuals = false;
+            Multithreaded = true;
+            GeneratedCodeModifier = EAccessModifier.Public;
+            Reporter = new Reporter();
         }
 
         public Report Execute()
         {
             try
             {
-                this.ExecuteBody();
+                ExecuteBody();
             }
             catch (Exception ex)
             {
-                this.reporter.Report(ex);
+                Reporter.Report(ex);
             }
 
-            this.ExecuteExportLog();
-            return this.Result;
+            ExecuteExportLog();
+            return Result;
         }
         
         // TODO: this method should really be private or internal, but this is just so nicer to test
@@ -144,7 +74,7 @@ namespace Hime.Parsers
             Grammar grammar = Execute_GetGrammar();
             if (grammar == null) return;
 
-            this.InitializeGenerator();
+            InitializeGenerator();
 
             ExecuteBuildData(grammar);
             ExecuteOpenOutput();
@@ -154,37 +84,37 @@ namespace Hime.Parsers
         
         private void ExecuteExportLog()
         {
-            if (!exportLog) return;
-            string file = this.ParserFile.Replace(".cs", "_log.mht");
-            reporter.ExportMHTML(file, "Grammar Log");
+            if (!ExportLog) return;
+            string file = ParserFile.Replace(".cs", "_log.mht");
+            Reporter.ExportMHTML(file, "Grammar Log");
         }
 
         // TODO: this method should really be private or internal, but this is just so nicer to test
         // TODO: think about it, it is a sign there is a small architectural problem there
         public bool ExecuteLoadData()
         {
-            if (fileInputs.Count == 0 && rawInputs.Count == 0)
+            if (InputFiles.Count == 0 && InputRawData.Count == 0)
             {
-                reporter.Error("Compiler", "No input!");
+                Reporter.Error("Compiler", "No input!");
                 return false;
             }
-            Hime.Kernel.Resources.ResourceCompiler compiler = new Hime.Kernel.Resources.ResourceCompiler(this.reporter);
+            Hime.Kernel.Resources.ResourceCompiler compiler = new Hime.Kernel.Resources.ResourceCompiler(Reporter);
             List<System.IO.TextReader> readers = new List<System.IO.TextReader>();
             // TODO: they are both streams => could be unified!!
-            foreach (string file in fileInputs)
+            foreach (string file in InputFiles)
             {
                 TextReader reader = new StreamReader(file);
                 readers.Add(reader);
                 compiler.AddInput(reader, file);
             }
-            foreach (string data in rawInputs)
+            foreach (string data in InputRawData)
             {
                 TextReader reader = new StringReader(data);
                 readers.Add(reader);
                 compiler.AddInput(reader);
             }
             bool result = compiler.Compile();
-            this.root = compiler.OutputRootNamespace;
+            root = compiler.OutputRootNamespace;
             foreach (System.IO.TextReader reader in readers)
                 reader.Close();
             return result;
@@ -194,16 +124,16 @@ namespace Hime.Parsers
         {
             Grammar grammar = null;
             // TODO: think about it, what if GrammarName is null => do a test
-            if (this.GrammarName != null)
+            if (GrammarName != null)
             {
-                try { grammar = (Hime.Parsers.Grammar)root.ResolveName(Hime.Kernel.QualifiedName.ParseName(this.GrammarName)); }
-                catch { reporter.Error("Compiler", "Cannot find grammar: " + this.GrammarName); }
+                try { grammar = (Hime.Parsers.Grammar)root.ResolveName(Hime.Kernel.QualifiedName.ParseName(GrammarName)); }
+                catch { Reporter.Error("Compiler", "Cannot find grammar: " + GrammarName); }
                 return grammar;
             }
             grammar = Execute_FindGrammar(root);
             if (grammar == null)
             {
-	            reporter.Error("Compiler", "Cannot find any grammar");
+                Reporter.Error("Compiler", "Cannot find any grammar");
             }
             return grammar;
         }
@@ -227,50 +157,50 @@ namespace Hime.Parsers
         
         private void InitializeGenerator()
         {
-            switch (this.Method)
+            switch (Method)
             {
                 case EParsingMethod.LR0:
-                    this.generator = new Hime.Parsers.ContextFree.LR.MethodLR0();
+                    ParserGenerator = new Hime.Parsers.ContextFree.LR.MethodLR0();
                     return;
                 case EParsingMethod.LR1:
-                    this.generator = new Hime.Parsers.ContextFree.LR.MethodLR1();
+                    ParserGenerator = new Hime.Parsers.ContextFree.LR.MethodLR1();
                     return;
                 case EParsingMethod.LALR1:
-                    this.generator = new Hime.Parsers.ContextFree.LR.MethodLALR1();
+                    ParserGenerator = new Hime.Parsers.ContextFree.LR.MethodLALR1();
                     return;
                 case EParsingMethod.LRStar:
-                    this.generator = new Hime.Parsers.ContextFree.LR.MethodLRStar();
+                    ParserGenerator = new Hime.Parsers.ContextFree.LR.MethodLRStar();
                     return;
                 case EParsingMethod.RNGLR1:
-                    this.generator = new Hime.Parsers.ContextFree.LR.MethodRNGLR1();
+                    ParserGenerator = new Hime.Parsers.ContextFree.LR.MethodRNGLR1();
                     return;
                 case EParsingMethod.RNGLALR1:
-                    this.generator = new Hime.Parsers.ContextFree.LR.MethodRNGLALR1();
+                    ParserGenerator = new Hime.Parsers.ContextFree.LR.MethodRNGLALR1();
                     return;
             }
-            string message = "Unsupported parsing method: " + this.Method.ToString();
-            reporter.Error("Compiler", message);
+            string message = "Unsupported parsing method: " + Method.ToString();
+            Reporter.Error("Compiler", message);
             throw new ArgumentException(message);
         }
         
         private void ExecuteBuildData(Grammar grammar)
         {
-            if (_namespace == null)
-                _namespace = grammar.CompleteName.ToString();
+            if (Namespace == null)
+                Namespace = grammar.CompleteName.ToString();
             // TODO: Shouldn't this be done earlier => as soon as the grammar name is determined
-            if (this.LexerFile == null)
+            if (LexerFile == null)
             {
-            	this.LexerFile = grammar.LocalName + "Lexer.cs";
+            	LexerFile = grammar.LocalName + "Lexer.cs";
             }
             // TODO: think about it, but should never be null?? Shouldn't this be done erarlier
-            if (this.ParserFile == null)
+            if (ParserFile == null)
             {
-            	this.ParserFile = grammar.LocalName + "Parser.cs";
+            	ParserFile = grammar.LocalName + "Parser.cs";
             }
             
-            docFile = null;
-            if (exportDoc)
-                docFile = this.ParserFile.Replace(".cs", "_doc.mht");
+            Documentation = null;
+            if (ExportDoc)
+                Documentation = ParserFile.Replace(".cs", "_doc.mht");
         }
         
         private StreamWriter OpenOutputStream(string fileName)
@@ -282,25 +212,25 @@ namespace Hime.Parsers
         
         private void ExecuteOpenOutput()
         {            
-            lexerWriter = OpenOutputStream(this.LexerFile);
-            if (this.ParserFile == this.LexerFile)
+            LexerWriter = OpenOutputStream(LexerFile);
+            if (ParserFile == LexerFile)
             {
-                this.parserWriter = this.lexerWriter;
+                ParserWriter = LexerWriter;
             }
             else
             {
-            	this.parserWriter = OpenOutputStream(this.ParserFile);
+                ParserWriter = OpenOutputStream(ParserFile);
             }
         }
 
         private void Execute_Close()
         {
-            lexerWriter.WriteLine("}");
-            lexerWriter.Close();
-            if (parserWriter != lexerWriter)
+            LexerWriter.WriteLine("}");
+            LexerWriter.Close();
+            if (ParserWriter != LexerWriter)
             {
-                parserWriter.WriteLine("}");
-                parserWriter.Close();
+                ParserWriter.WriteLine("}");
+                ParserWriter.Close();
             }
         }
 
