eb1eab56fc34a3dc13bd728ac6b5be3cff2041c0
Charles Hymans <charles.hymans@wanadoo.fr>
2011-11-01 10:49:42 +0000

    [clean] Added missing call to Dispose.

--- Exe.HimeDemo/Tasks/ANSICParse.cs
+++ Exe.HimeDemo/Tasks/ANSICParse.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿/*
+ * Author: Charles Hymans
+ * */
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using Hime.Redist.Parsers;
 
 namespace Hime.Demo.Tasks
 {
@@ -8,16 +13,22 @@ namespace Hime.Demo.Tasks
     {
         public void Execute()
         {
-            Analyser.ANSI_C_Lexer lexer = new Analyser.ANSI_C_Lexer(new System.IO.StreamReader("test.c"));
+            Analyser.ANSI_C_Lexer lexer = new Analyser.ANSI_C_Lexer(new StreamReader("test.c"));
             Analyser.ANSI_C_Parser parser = new Analyser.ANSI_C_Parser(lexer);
-            Hime.Redist.Parsers.SyntaxTreeNode root = parser.Analyse();
+            SyntaxTreeNode root = parser.Analyse();
 
-            foreach (Hime.Redist.Parsers.ParserError error in parser.Errors)
-                System.Console.WriteLine(error.ToString());
+            foreach (ParserError error in parser.Errors)
+			{
+                Console.WriteLine(error.ToString());
+			}
+			
             if (root != null)
             {
-                LangTest.WinTreeView window = new LangTest.WinTreeView(root);
-                window.ShowDialog();
+				// TODO: what is this LangTest namespace, why is it different?
+				using (LangTest.WinTreeView window = new LangTest.WinTreeView(root))
+				{
+                	window.ShowDialog();
+				}
             }
         }
     }
