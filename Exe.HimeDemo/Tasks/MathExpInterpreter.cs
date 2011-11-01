806bb52da149086efc1fff16518455a5f0b3d375
Charles Hymans <charles.hymans@wanadoo.fr>
2011-11-01 11:15:14 +0000

    [clean]

--- Exe.HimeDemo/Tasks/MathExpInterpreter.cs
+++ Exe.HimeDemo/Tasks/MathExpInterpreter.cs
@@ -1,51 +1,55 @@
-﻿using System;
+﻿/*
+ * Author: Charles Hymans
+ * */
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Hime.Redist.Parsers;
 
 namespace Hime.Demo.Tasks
 {
-    class MathExpInterpreter : Analyser.MathExp_Parser.Actions
+    internal class MathExpInterpreter : Analyser.MathExp_Parser.Actions
+    {
+		private Stack<float> stack;
+
+        internal float Value { get { return stack.Peek(); } }
+
+        internal MathExpInterpreter() { stack = new System.Collections.Generic.Stack<float>(); }
+
+        internal void OnNumber(SyntaxTreeNode SubRoot)
         {
-            private System.Collections.Generic.Stack<float> stack;
-
-            public float Value { get { return stack.Peek(); } }
-
-            public MathExpInterpreter() { stack = new System.Collections.Generic.Stack<float>(); }
-
-            public void OnNumber(Hime.Redist.Parsers.SyntaxTreeNode SubRoot)
-            {
-                Hime.Redist.Parsers.SyntaxTreeNode node = SubRoot.Children[0];
-                Hime.Redist.Parsers.SymbolTokenText token = (Hime.Redist.Parsers.SymbolTokenText)node.Symbol;
-                float value = System.Convert.ToSingle(token.ValueText);
-                stack.Push(value);
-            }
-
-            public void OnMult(Hime.Redist.Parsers.SyntaxTreeNode SubRoot)
-            {
-                float right = stack.Pop();
-                float left = stack.Pop();
-                stack.Push(left * right);
-            }
-
-            public void OnDiv(Hime.Redist.Parsers.SyntaxTreeNode SubRoot)
-            {
-                float right = stack.Pop();
-                float left = stack.Pop();
-                stack.Push(left / right);
-            }
-
-            public void OnPlus(Hime.Redist.Parsers.SyntaxTreeNode SubRoot)
-            {
-                float right = stack.Pop();
-                float left = stack.Pop();
-                stack.Push(left + right);
-            }
-
-            public void OnMinus(Hime.Redist.Parsers.SyntaxTreeNode SubRoot)
-            {
-                float right = stack.Pop();
-                float left = stack.Pop();
-                stack.Push(left - right);
-            }
+        	SyntaxTreeNode node = SubRoot.Children[0];
+            SymbolTokenText token = (SymbolTokenText)node.Symbol;
+            float value = Convert.ToSingle(token.ValueText);
+            stack.Push(value);
         }
+
+        internal void OnMult(SyntaxTreeNode SubRoot)
+        {
+        	float right = stack.Pop();
+            float left = stack.Pop();
+            stack.Push(left * right);
+        }
+
+        internal void OnDiv(SyntaxTreeNode SubRoot)
+        {
+            float right = stack.Pop();
+            float left = stack.Pop();
+            stack.Push(left / right);
+        }
+
+        internal void OnPlus(SyntaxTreeNode SubRoot)
+        {
+            float right = stack.Pop();
+            float left = stack.Pop();
+            stack.Push(left + right);
+        }
+
+        internal void OnMinus(SyntaxTreeNode SubRoot)
+        {
+            float right = stack.Pop();
+            float left = stack.Pop();
+            stack.Push(left - right);
+        }
+	}
 }
