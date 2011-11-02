249562517b438c40a0c6605b454230be3883faa0
Charles Hymans <charles.hymans@wanadoo.fr>
2011-11-02 07:54:16 +0000

    [clean]

--- Lib.CentralDogma/Parsers/ContextFree/MethodLR/GLALR1/StateReductionsGLALR1.cs
+++ Lib.CentralDogma/Parsers/ContextFree/MethodLR/GLALR1/StateReductionsGLALR1.cs
@@ -23,35 +23,35 @@ namespace Hime.Parsers.ContextFree.LR
 
         public StateReductionsGLALR1() : base() { }
 
-        public override void Build(State Set)
+        public override void Build(State state)
         {
             // Build shift actions
-            foreach (GrammarSymbol Next in Set.Children.Keys)
+            foreach (GrammarSymbol Next in state.Children.Keys)
             {
                 List<StateAction> Actions = new List<StateAction>();
-                Actions.Add(new StateActionShift(Next, Set.Children[Next]));
+                Actions.Add(new StateActionShift(Next, state.Children[Next]));
             }
 
             // Recutions dictionnary for the given set
             Dictionary<Terminal, ItemLALR1> Reductions = new Dictionary<Terminal, ItemLALR1>();
             // Construct reductions
-            foreach (ItemLALR1 Item in Set.Items)
+            foreach (ItemLALR1 Item in state.Items)
             {
                 if (Item.Action == ItemAction.Shift)
                     continue;
                 foreach (Terminal Lookahead in Item.Lookaheads)
                 {
                     // There is already a shift action for the lookahead => conflict
-                    if (Set.Children.ContainsKey(Lookahead))
+                    if (state.Children.ContainsKey(Lookahead))
                     {
-                        StateReductionsLR1.HandleConflict_ShiftReduce("GLALR(1)", conflicts, Item, Set, Lookahead);
+                        StateReductionsLR1.HandleConflict_ShiftReduce("GLALR(1)", conflicts, Item, state, Lookahead);
                         StateActionReduce Reduction = new StateActionReduce(Lookahead, Item.BaseRule);
                         this.Add(Reduction);
                     }
                     // There is already a reduction action for the lookahead => conflict
                     else if (Reductions.ContainsKey(Lookahead))
                     {
-                        StateReductionsLR1.HandleConflict_ReduceReduce("GLALR(1)", conflicts, Item, Reductions[Lookahead], Set, Lookahead);
+                        StateReductionsLR1.HandleConflict_ReduceReduce("GLALR(1)", conflicts, Item, Reductions[Lookahead], state, Lookahead);
                         StateActionReduce Reduction = new StateActionReduce(Lookahead, Item.BaseRule);
                         this.Add(Reduction);
                     }
