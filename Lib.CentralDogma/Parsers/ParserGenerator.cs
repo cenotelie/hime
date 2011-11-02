60a2473205c67ed476a88add4150dbd30a601de8
Charles Hymans <charles.hymans@wanadoo.fr>
2011-11-02 07:32:22 +0000

    [clean]

--- Lib.CentralDogma/Parsers/ParserGenerator.cs
+++ Lib.CentralDogma/Parsers/ParserGenerator.cs
@@ -5,12 +5,17 @@
  * 
  */
 using Hime.Kernel.Reporting;
+using Hime.Parsers.ContextFree;
 
 namespace Hime.Parsers
 {
+	// TODO: replace this by BaseMethod and rename BaseMethod into ParserGenerator
     public interface ParserGenerator
     {
+		// TODO: check Name is really necessary?
         string Name { get; }
+		// TODO: remove this method
         ParserData Build(Grammar grammar, Reporter reporter);
+		ParserData Build(CFGrammar grammar);
     }
 }
