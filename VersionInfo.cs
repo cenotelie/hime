9cb91e150785e14b6ac1d3f9326fc5ff31102da3
Laurent Wouters <lwouters@cenotelie.fr>
2013-03-12 09:00:29 +0100

    Updated assembly properties (version 0.6.0.0)

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
