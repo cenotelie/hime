082443b863789a52e9c0e1be452733b36d5b1f30
Laurent Wouters <lwouters@cenotelie.fr>
2013-05-13 10:32:28 +0200

    Added XML comments in Redist.Utils

--- Lib.Redist/Utils/MemoryMaps.cs
+++ Lib.Redist/Utils/MemoryMaps.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace Hime.Redist.Utils
 {
+    /// <summary>
+    /// Represents a blob of binary data that can be accessed as an array of signed 32-bits integers
+    /// </summary>
     [StructLayout(LayoutKind.Explicit)]
     class BlobInt
     {
@@ -15,13 +15,28 @@ namespace Hime.Redist.Utils
         [FieldOffset(8)]
         private int sizeBlob;
 
+        /// <summary>
+        /// Gets the raw array of bytes
+        /// </summary>
         public byte[] Raw { get { return blob; } }
+        /// <summary>
+        /// Gets the size of the binary blob in bytes
+        /// </summary>
         public int RawSize { get { return sizeBlob; } }
+        /// <summary>
+        /// Gets the 32-bits signed integer at the given index
+        /// </summary>
+        /// <param name="index">Index of the integer</param>
+        /// <returns>The 32-bits signed integer at the given index</returns>
         public int this[int index]
         {
             get { return data[index]; }
         }
 
+        /// <summary>
+        /// Initializes a new blob with the given size in bytes
+        /// </summary>
+        /// <param name="sizeBlob">The size of the blob in bytes</param>
         public BlobInt(int sizeBlob)
         {
             this.blob = new byte[sizeBlob];
@@ -29,6 +44,9 @@ namespace Hime.Redist.Utils
         }
     }
 
+    /// <summary>
+    /// Represents a blob of binary data that can be accessed as an array of unsigned 16-bits integers
+    /// </summary>
     [StructLayout(LayoutKind.Explicit)]
     class BlobUShort
     {
@@ -39,14 +57,32 @@ namespace Hime.Redist.Utils
         [FieldOffset(8)]
         private int sizeBlob;
 
+        /// <summary>
+        /// Gets the raw array of bytes
+        /// </summary>
         public byte[] Raw { get { return blob; } }
+        /// <summary>
+        /// Gets the size of the binary blob in bytes
+        /// </summary>
         public int RawSize { get { return sizeBlob; } }
+        /// <summary>
+        /// Gets the number of unsigned 16-bits integer in this blob
+        /// </summary>
         public int Length { get { return sizeBlob / 2; } }
+        /// <summary>
+        /// Gets the 16-bits unsigned integer at the given index
+        /// </summary>
+        /// <param name="index">Index of the integer</param>
+        /// <returns>The 16-bits unsigned integer at the given index</returns>
         public ushort this[int index]
         {
             get { return data[index]; }
         }
 
+        /// <summary>
+        /// Initializes a new blob with the given size in bytes
+        /// </summary>
+        /// <param name="sizeBlob">The size of the blob in bytes</param>
         public BlobUShort(int sizeBlob)
         {
             this.blob = new byte[sizeBlob];
--- Lib.Redist/Utils/SIDHashMap.cs
+++ Lib.Redist/Utils/SIDHashMap.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Hime.Redist.Utils
 {
+    /// <summary>
+    /// Optimized hashmap for data accessed by ids from 0x0000 to 0x01FF
+    /// </summary>
+    /// <typeparam name="T">The type of data to be stored</typeparam>
     class SIDHashMap<T>
     {
+        /// <summary>
+        /// Cache for ids from 0x00 to 0xFF
+        /// </summary>
         private T[] cache1;
+        /// <summary>
+        /// Cache for ids from 0x100 to 0x1FF
+        /// </summary>
         private T[] cache2;
+        /// <summary>
+        /// Hashmap for the other ids
+        /// </summary>
         private Dictionary<ushort, T> others;
 
+        /// <summary>
+        /// Initializes the structure
+        /// </summary>
         public SIDHashMap()
         {
             cache1 = new T[256];
         }
 
+        /// <summary>
+        /// Adds a new data in the collection with the given key
+        /// </summary>
+        /// <param name="key">The key for the data</param>
+        /// <param name="value">The data</param>
         public void Add(ushort key, T value)
         {
             if (key <= 0xFF)
@@ -33,6 +53,11 @@ namespace Hime.Redist.Utils
             }
         }
 
+        /// <summary>
+        /// Gets the data for the given key
+        /// </summary>
+        /// <param name="key">The key for the data</param>
+        /// <returns>The data corresponding to the key</returns>
         public T this[ushort key]
         {
             get
@@ -42,10 +67,6 @@ namespace Hime.Redist.Utils
                 else if (key <= 0x1FF)
                     return cache2[key];
                 return others[key];
-            }
-            set
-            {
-                throw new NotImplementedException();
             }
         }
     }
