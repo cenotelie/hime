89aad519589f1c03cbe74a24b2c8caefb8ff4706
Laurent Wouters <lwouters@cenotelie.fr>
2014-06-04 17:27:43 +0200

    [releng] Added Windows script for nuget package building

---
+++ releng/nuget/package.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set OUTPUT=releng/nuget
+
+nuget pack runtimes/net/Hime.Redist.csproj -OutputDirectory %OUTPUT% -Build -Symbols -Properties Configuration=Release
+nuget pack core/Hime.SDK.csproj -OutputDirectory %OUTPUT% -Build -Symbols -IncludeReferencedProjects -Properties Configuration=Release
