2c22f8a5e4f30be4fae5d5dad25ab37275ad9548
Laurent Wouters <lwouters@cenotelie.fr>
2014-09-30 13:38:26 +0200

    [releng] Moving to version 2.0 for API breaking changes

--- releng/VersionInfo.cs
+++ releng/VersionInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
