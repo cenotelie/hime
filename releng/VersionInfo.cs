6f415101f1f871aa95cf5b0c4e2cd80e621f3881
Laurent Wouters <lwouters@cenotelie.fr>
2014-10-09 09:46:35 +0200

    [releng] Mooving to version 1.4.0

--- releng/VersionInfo.cs
+++ releng/VersionInfo.cs
@@ -23,5 +23,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
