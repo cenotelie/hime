ef84964d73feaa4871223a6a801057954176b333
Laurent Wouters <lwouters@cenotelie.fr>
2015-05-30 09:36:34 +0200

    [clean] Clean-up tree view window in demo

--- utilities/net/demo/WinTreeView.cs
+++ utilities/net/demo/WinTreeView.cs
@@ -26,42 +26,31 @@ namespace Hime.Demo
 	/// <summary>
 	/// Simple window that displays an AST
 	/// </summary>
-    partial class WinTreeView : Form
-    {
+	partial class WinTreeView : Form
+	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Hime.Demo.WinTreeView"/> class.
 		/// </summary>
 		/// <param name="root">The root of the AST to display</param>
-        public WinTreeView(ASTNode root)
-        {
-            InitializeComponent();
-            TreeNode vroot = View.Nodes.Add(GetString(root));
-            AddSubTree(vroot, root);
-        }
+		public WinTreeView(ASTNode root)
+		{
+			InitializeComponent();
+			TreeNode vroot = View.Nodes.Add(root.ToString());
+			AddSubTree(vroot, root);
+		}
 
 		/// <summary>
 		/// Recursively adds the given AST node and its children to the tree viewer
 		/// </summary>
 		/// <param name="vnode">The current tree viewer node</param>
 		/// <param name="snode">An AST node</param>
-        private void AddSubTree(TreeNode vnode, ASTNode snode)
-        {
-            foreach (ASTNode child in snode.Children)
-            {
-                TreeNode vchild = vnode.Nodes.Add(GetString(child));
-                AddSubTree(vchild, child);
-            }
-        }
-
-		/// <summary>
-		/// Gets the string representation for display of the given node
-		/// </summary>
-		/// <param name="node">An AST node</param>
-		/// <returns>The string representation of the node</returns>
-        private string GetString(ASTNode node)
-        {
-			string value = node.Value;
-			return (value ?? node.Symbol.Name);
-        }
-    }
+		private static void AddSubTree(TreeNode vnode, ASTNode snode)
+		{
+			foreach (ASTNode child in snode.Children)
+			{
+				TreeNode vchild = vnode.Nodes.Add(child.ToString());
+				AddSubTree(vchild, child);
+			}
+		}
+	}
 }
