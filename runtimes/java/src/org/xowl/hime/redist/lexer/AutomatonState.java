9c40be0b9009fc6a3628701351382bb8076122b7
Laurent Wouters <lwouters@cenotelie.fr>
2015-05-30 09:55:54 +0200

    [clean] Updated copyright in Java runtime

--- runtimes/java/src/org/xowl/hime/redist/lexer/AutomatonState.java
+++ runtimes/java/src/org/xowl/hime/redist/lexer/AutomatonState.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Laurent Wouters and others
+ * Copyright (c) 2015 Laurent Wouters
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3
@@ -22,7 +22,7 @@ package org.xowl.hime.redist.lexer;
 
 /**
  * Represents a state in the automaton of a lexer
- *
+ * <p>
  * Binary data structure:
  * uint16: number of matched terminals
  * uint16: total number of transitions
--- runtimes/java/src/org/xowl/hime/redist/parsers/LRkAutomaton.java
+++ runtimes/java/src/org/xowl/hime/redist/parsers/LRkAutomaton.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Laurent Wouters and others
+ * Copyright (c) 2015 Laurent Wouters
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3
