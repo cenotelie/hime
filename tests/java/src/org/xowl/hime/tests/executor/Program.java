902777ff1a50279b1a24cea4ac85f8815f50f97b
Laurent Wouters <lwouters@cenotelie.fr>
2017-03-22 16:04:25 +0100

    [releng] Updating copyright to Cenotelie

--- tests/java/src/org/xowl/hime/tests/executor/Program.java
+++ tests/java/src/org/xowl/hime/tests/executor/Program.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Laurent Wouters and others
+ * Copyright (c) 2017 Association Cénotélie (cenotelie.fr)
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3
@@ -13,10 +13,8 @@
  * You should have received a copy of the GNU Lesser General
  * Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
- *
- * Contributors:
- *     Laurent Wouters - lwouters@xowl.org
  ******************************************************************************/
+
 package org.xowl.hime.tests.executor;
 
 import org.xowl.hime.redist.ASTNode;
