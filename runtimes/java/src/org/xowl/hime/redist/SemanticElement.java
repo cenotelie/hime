563df7e87916d394f9120e3b5263dfab98bb5e6a
Laurent Wouters <lwouters@cenotelie.fr>
2017-03-22 15:36:08 +0100

    [releng] Updating copyright to Cenotelie

--- runtimes/java/src/org/xowl/hime/redist/SemanticElement.java
+++ runtimes/java/src/org/xowl/hime/redist/SemanticElement.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Laurent Wouters
+ * Copyright (c) 2017 Association Cénotélie (cenotelie.fr)
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3
@@ -13,9 +13,6 @@
  * You should have received a copy of the GNU Lesser General
  * Public License along with this program.
  * If not, see <http://www.gnu.org/licenses/>.
- *
- * Contributors:
- *     Laurent Wouters - lwouters@xowl.org
  ******************************************************************************/
 package org.xowl.hime.redist;
 
