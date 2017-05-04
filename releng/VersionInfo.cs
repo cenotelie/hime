05fcc957812d3a1710ab9b53f2a59b4e263216f7
Laurent Wouters <lwouters@cenotelie.fr>
2017-05-04 21:54:36 +0200

    [releng] Preparing v3.0.0

--- releng/VersionInfo.cs
+++ releng/VersionInfo.cs
@@ -19,5 +19,5 @@
 using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Association Cénotélie 2017")]
-[assembly: AssemblyVersion("2.0.7.0")]
-[assembly: AssemblyFileVersion("2.0.7.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
