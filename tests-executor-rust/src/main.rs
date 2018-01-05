5f7f58fdfe3564c467208a4704b6cd4ed50a9514
Laurent Wouters <lwouters@cenotelie.fr>
2018-01-05 16:29:26 +0100

    [feature] Implementing the Rust runtime

--- tests-executor-rust/src/main.rs
+++ tests-executor-rust/src/main.rs
@@ -20,7 +20,40 @@
 extern crate hime_redist;
 extern crate libloading;
 
+use std::env;
+use std::process;
+
+/// The parser must produce an AST that matches the expected one
+const VERB_MATCHES: &'static str = "matches";
+/// The parser must produce an AST that do NOT match the expected one
+const VERB_NOMATCHES: &'static str = "nomatches";
+/// The parser must fail
+const VERB_FAILS: &'static str = "fails";
+/// The parser have the specified output
+const VERB_OUTPUTS: &'static str = "outputs";
+
+/// The test was successful
+const RESULT_SUCCESS: i32 = 0;
+/// The test failed in the end
+const RESULT_FAILURE_VERB: i32 = 1;
+/// The test failed in its parsing phase
+const RESULT_FAILURE_PARSING: i32 = 2;
+
 /// Main entry point
 fn main() {
-    println!("Hello, world!");
+    {
+        let args = read_args();
+    }
+    process::exit(RESULT_FAILURE_PARSING);
+}
+
+/// Reads the arguments
+fn read_args() -> Vec<String> {
+    let mut args = Vec::<String>::new();
+    for argument in env::args().enumerate() {
+        if argument.0 != 0 {
+            args.push(argument.1);
+        }
+    }
+    args
 }
