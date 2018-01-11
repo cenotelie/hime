ddaaa0aed9404b3433efd41ad8234a779439c589
Laurent Wouters <lwouters@cenotelie.fr>
2018-01-11 08:14:29 +0100

    [feature] Implementing the Rust runtime

--- runtime-rust/src/parsers/rnglr.rs
+++ runtime-rust/src/parsers/rnglr.rs
@@ -32,21 +32,169 @@ use super::super::symbols::SemanticBody;
 use super::super::symbols::SemanticElement;
 use super::super::symbols::SemanticElementTrait;
 
+/// Represents a cell in a RNGLR parse table
+#[derive(Copy, Clone)]
+struct RNGLRAutomatonCell {
+    /// The number of actions in this cell
+    count: u32,
+    /// Index of the cell's data
+    index: u32
+}
+
 /// Represents the RNGLR parsing table and productions
 pub struct RNGLRAutomaton {
+    /// Index of the axiom variable
+    axiom: usize,
     /// The number of columns in the LR table
     columns_count: usize,
     /// The number of states in the LR table
-    states_count: usize
+    states_count: usize,
+    /// Map of symbol ID to column index in the LR table
+    columns_map: LRColumnMap,
+    /// The contexts information
+    contexts: Vec<LRContexts>,
+    /// The RNGLR table
+    cells: Vec<RNGLRAutomatonCell>,
+    /// The LR action table
+    table: Vec<u16>,
+    /// The table of LR productions
+    productions: Vec<LRProduction>,
+    /// The table of nullable variables
+    nullables: Vec<u16>
 }
 
 impl RNGLRAutomaton {
     /// Initializes a new automaton from the given binary data
     pub fn new(data: &[u8]) -> RNGLRAutomaton {
-        RNGLRAutomaton {
-            columns_count: 0,
-            states_count: 0
+        // read basic counters
+        let axiom_index = read_u16(data, 0) as usize;
+        let columns_count = read_u16(data, 2) as usize;
+        let states_count = read_u16(data, 4) as usize;
+        let actions_count = read_u32(data, 6) as usize;
+        let productions_count = read_u16(data, 10) as usize;
+        let nullables_count = read_u16(data, 12) as usize;
+        // reads the column map
+        let columns_map = LRColumnMap::new(data, 14, columns_count);
+        // read the contexts table
+        let mut contexts = Vec::<LRContexts>::with_capacity(states_count);
+        let mut index = 14 + columns_count * 2;
+        for _i in 0..states_count {
+            let mut context = LRContexts::new();
+            let count = read_u16(data, index);
+            index += 2;
+            for _j in 0..count {
+                context.add(read_u16(data, index), read_u16(data, index + 2));
+                index += 4
+            }
+            contexts.push(context);
         }
+        // read the automaton cells
+        let mut cells = Vec::<RNGLRAutomatonCell>::with_capacity(columns_count * states_count);
+        for _i in 0..(columns_count * states_count) {
+            cells.push(RNGLRAutomatonCell {
+                count: read_u16(data, index) as u32,
+                index: read_u32(data, index + 2)
+            });
+            index += 6;
+        }
+        // read the actions table for the automaton
+        let table = read_table_u16(data, index, actions_count * 2);
+        index += actions_count * 4;
+        // read the production table
+        let mut productions = Vec::<LRProduction>::with_capacity(productions_count);
+        for _i in 0..productions_count {
+            let production = LRProduction::new(data, &mut index);
+            productions.push(production);
+        }
+        // read the nullables table
+        let nullables = read_table_u16(data, index, nullables_count);
+        index += nullables_count * 2;
+        assert_eq!(index, data.len());
+        RNGLRAutomaton {
+            axiom: axiom_index,
+            columns_count,
+            states_count,
+            columns_map,
+            contexts,
+            cells,
+            table,
+            productions,
+            nullables
+        }
+    }
+
+    /// Gets the index of the axiom
+    pub fn get_axiom(&self) -> usize {
+        self.axiom
+    }
+
+    /// Gets the number of states in this automaton
+    pub fn get_states_count(&self) -> usize {
+        self.states_count
+    }
+
+    /// Gets the contexts opened by the specified state
+    pub fn get_contexts(&self, state: u32) -> &LRContexts {
+        &self.contexts[state as usize]
+    }
+
+    /// Gets the number of GLR actions for the given state and symbol identifier
+    pub fn get_actions_count(&self, state: u32, identifier: u32) -> usize {
+        let column = self.columns_map.get(identifier) as usize;
+        self.cells[state as usize * self.columns_count + column].count as usize
+    }
+
+    /// Gets the i-th GLR action for the given state and sid
+    pub fn get_action(&self, state: u32, identifier: u32, index: usize) -> LRAction {
+        let column = self.columns_map.get(identifier) as usize;
+        let cell = self.cells[state as usize * self.columns_count + column];
+        LRAction {
+            table: &self.table,
+            offset: (cell.index as usize + index) * 2
+        }
+    }
+
+    /// Gets the i-th production
+    pub fn get_production(&self, index: usize) -> &LRProduction {
+        &self.productions[index]
+    }
+
+    /// Gets the production for the nullable variable with the given index
+    pub fn get_nullable_production(&self, index: usize) -> Option<&LRProduction> {
+        match self.nullables[index] {
+            0xFFFF => None,
+            prod_index => Some(&self.productions[prod_index as usize])
+        }
+    }
+
+    /// Determine whether the given state is the accepting state
+    pub fn is_accepting_state(&self, state: u32) -> bool {
+        let cell = self.cells[state as usize * self.columns_count];
+        if cell.count != 1 {
+            false
+        } else {
+            self.table[(cell.index as usize) * 2] == LR_ACTION_CODE_ACCEPT
+        }
+    }
+
+    /// Gets the expected terminals for the specified state
+    pub fn get_expected(&self, state: u32, terminals: &'static [Symbol]) -> LRExpected {
+        let mut expected = LRExpected::new();
+        for (column, terminal) in terminals.iter().enumerate() {
+            let cell = self.cells[state as usize * self.columns_count + column];
+            for i in 0..cell.count as usize {
+                let action = LRAction {
+                    table: &self.table,
+                    offset: (cell.index as usize + i) * 2
+                };
+                if action.get_code() == LR_ACTION_CODE_SHIFT {
+                    expected.add_unique_shift(*terminal);
+                } else if action.get_code() == LR_ACTION_CODE_REDUCE {
+                    expected.add_unique_reduction(*terminal);
+                }
+            }
+        }
+        expected
     }
 }
 
