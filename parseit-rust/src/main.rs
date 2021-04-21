27e378159774dca363d681785533f3aa1b5d0e11
Laurent Wouters <lwouters@cenotelie.fr>
2021-04-21 09:52:46 +0200

    Modernize Rust

--- parseit-rust/src/main.rs
+++ parseit-rust/src/main.rs
@@ -17,23 +17,44 @@
 
 //! Main module
 
-extern crate hime_redist;
-extern crate libloading;
-extern crate serde_json;
-
 use std::{env, io};
 
+use clap::{App, Arg};
 use hime_redist::result::ParseResult;
+
+/// The version of this program
+pub const CRATE_VERSION: &str = env!("CARGO_PKG_VERSION");
+/// The commit that was used to build the application
+pub const GIT_HASH: &str = env!("GIT_HASH");
+/// The git tag that was used to build the application
+pub const GIT_TAG: &str = env!("GIT_TAG");
 
 /// Main entry point
 fn main() {
-    let args = read_args();
-    if args.len() != 2 {
-        print_help();
-        return;
-    }
+    let matches = App::new("Hime ParseIt")
+        .version(format!("{} tag={} hash={}", CRATE_VERSION, GIT_TAG, GIT_HASH).as_str())
+        .author("Association Cénotélie <contact@cenotelie.fr>")
+        .about("Command line to parse a piece of input using a packaged parser.")
+        .arg(
+            Arg::with_name("library")
+                .value_name("LIBRARY")
+                .help("Path to the compiled library containing the parser")
+                .takes_value(true)
+                .required(true)
+        )
+        .arg(
+            Arg::with_name("module")
+                .value_name("MODULE")
+                .help("The module inside the library that contains the parser")
+                .takes_value(true)
+                .required(true)
+        )
+        .get_matches();
+
+    let library = matches.value_of("library").unwrap();
+    let module = matches.value_of("module").unwrap();
     let mut input = io::stdin();
-    do_parse(&mut input, &args[0], &args[1]);
+    do_parse(&mut input, library, module);
 }
 
 /// Parses the input
@@ -41,25 +62,11 @@ fn do_parse(input: &mut dyn io::Read, lib_name: &str, parser_module: &str) {
     let mut function_name = String::new();
     function_name.push_str(parser_module);
     function_name.push_str("_parse_utf8");
-    let library = libloading::Library::new(lib_name).unwrap_or_else(|error| panic!("{}", error));
     unsafe {
-        let parser: libloading::Symbol<fn(&mut dyn io::Read) -> ParseResult> = library
-            .get(function_name.as_bytes())
-            .unwrap_or_else(|error| panic!("{}", error));
+        let library = libloading::Library::new(lib_name).unwrap();
+        let parser: libloading::Symbol<fn(&mut dyn io::Read) -> ParseResult> =
+            library.get(function_name.as_bytes()).unwrap();
         let result = parser(input);
         serde_json::to_writer(std::io::stdout(), &result).unwrap()
     }
-}
-
-/// Reads the arguments
-fn read_args() -> Vec<String> {
-    env::args().skip(1).collect()
-}
-
-/// Prints the help screen for this program
-fn print_help() {
-    println!("parseit {} (LGPL 3)", env!("CARGO_PKG_VERSION"));
-    println!("Command line to parse a piece of input using a packaged parser");
-    println!();
-    println!("usage: parseit <parser_library> <parser_module>");
 }
