c4c19e38f550201eba0536fa787530c589c194a7
Laurent Wouters <lwouters@cenotelie.fr>
2021-04-24 19:58:08 +0200

    Migrating test driver to Rust

---
+++ tests-driver/src/loaders/mod.rs
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Association Cénotélie (cenotelie.fr)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+//! Loaders for test data
+
+use std::io::BufReader;
+
+#[allow(dead_code)]
+mod expected_tree;
+#[allow(dead_code)]
+mod fixture;
+
+pub use fixture::{
+    ID_VARIABLE_TEST_FAILS, ID_VARIABLE_TEST_MATCHES, ID_VARIABLE_TEST_NO_MATCH,
+    ID_VARIABLE_TEST_OUTPUT
+};
+use hime_redist::errors::ParseErrorDataTrait;
+use hime_redist::result::ParseResult;
+use hime_sdk::errors::{Error, Errors};
+use hime_sdk::{InputReference, LoadedData, LoadedInput};
+
+use crate::model::{Fixture, FixtureDef, Fixtures};
+
+/// Parses all fixtures
+pub fn parse_fixtures(fixtures: &[FixtureDef]) -> Result<Fixtures, Errors> {
+    let results: Vec<ParseResult> = fixtures
+        .iter()
+        .map(|FixtureDef(_, content)| {
+            let mut reader = BufReader::new(*content);
+            fixture::parse_utf8(&mut reader)
+        })
+        .collect();
+    let is_ok = results.iter().all(|r| r.is_success());
+    if is_ok {
+        Ok(Fixtures(
+            results.into_iter().map(Fixture::from_content).collect()
+        ))
+    } else {
+        let errors: Vec<Error> = results
+            .iter()
+            .enumerate()
+            .map(|(index, r)| {
+                r.errors.errors.iter().map(move |error| {
+                    let position = error.get_position();
+                    Error::Parsing(
+                        InputReference {
+                            input_index: index,
+                            position,
+                            length: error.get_length()
+                        },
+                        error.get_message()
+                    )
+                })
+            })
+            .flatten()
+            .collect();
+        Err(Errors {
+            data: LoadedData {
+                inputs: results
+                    .into_iter()
+                    .zip(fixtures.iter())
+                    .map(|(r, FixtureDef(name, _))| LoadedInput {
+                        name: name.to_string(),
+                        content: r.text
+                    })
+                    .collect(),
+                grammars: Vec::new()
+            },
+            errors
+        })
+    }
+}
---
+++ tests-driver/src/main.rs
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Association Cénotélie (cenotelie.fr)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+//! Main module
+
+use hime_sdk::errors::Errors;
+use model::FixtureDef;
+
+mod loaders;
+mod model;
+
+/// The name of this program
+pub const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
+/// The version of this program
+pub const CRATE_VERSION: &str = env!("CARGO_PKG_VERSION");
+/// The commit that was used to build the application
+pub const GIT_HASH: &str = env!("GIT_HASH");
+/// The git tag that was used to build the application
+pub const GIT_TAG: &str = env!("GIT_TAG");
+
+const FIXTURES: [FixtureDef; 8] = [
+    FixtureDef(
+        "ContextSensitive",
+        include_bytes!("fixtures/ContextSensitive.suite")
+    ),
+    FixtureDef("Errors", include_bytes!("fixtures/Errors.suite")),
+    FixtureDef(
+        "GrammarOptions",
+        include_bytes!("fixtures/GrammarOptions.suite")
+    ),
+    FixtureDef(
+        "LexicalRules",
+        include_bytes!("fixtures/LexicalRules.suite")
+    ),
+    FixtureDef("Regressions", include_bytes!("fixtures/Regressions.suite")),
+    FixtureDef(
+        "SyntacticRules",
+        include_bytes!("fixtures/SyntacticRules.suite")
+    ),
+    FixtureDef("TreeActions", include_bytes!("fixtures/TreeActions.suite")),
+    FixtureDef(
+        "UnicodeBlocks",
+        include_bytes!("fixtures/UnicodeBlocks.suite")
+    )
+];
+
+/// Main entry point
+fn main() {
+    let fixtures = on_errors(loaders::parse_fixtures(&FIXTURES));
+    on_errors(fixtures.build());
+}
+
+/// Handle errors
+fn on_errors<T>(result: Result<T, Errors>) -> T {
+    match result {
+        Ok(r) => r,
+        Err(errors) => {
+            hime_sdk::errors::print::print_errors(&errors);
+            std::process::exit(1);
+        }
+    }
+}
