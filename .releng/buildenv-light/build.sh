d6251e036b8fc41fd59ed3c78e64cbb982091c21
Laurent Wouters <lwouters@cenotelie.fr>
2021-12-05 18:48:00 +0100

    Prepare light build environment

---
+++ .releng/buildenv-light/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT="$(readlink -f "$0")"
+DIR="$(dirname "$SCRIPT")"
+RELENG="$(dirname "$DIR")"
+ROOT="$(dirname "$RELENG")"
+
+HASH=$(git rev-parse HEAD)
+
+IMAGES=$(docker images | grep -o -E '^cenotelie/hime-build-env-light(\s)+latest' | wc -l)
+# if [ "$IMAGES" -lt "1" ]; then
+  echo "=> Building cenotelie/hime-build-env-light:latest"
+  docker build --tag "cenotelie/hime-build-env-light:latest" --rm  --label changeset="$HASH" "$DIR"
+# fi
--- .releng/buildenv/build.sh
+++ .releng/buildenv/build.sh
@@ -7,7 +7,6 @@ ROOT="$(dirname "$RELENG")"
 
 HASH=$(git rev-parse HEAD)
 
-# Check for nexus.agricol.io/build-env
 IMAGES=$(docker images | grep -o -E '^cenotelie/hime-build-env(\s)+latest' | wc -l)
 # if [ "$IMAGES" -lt "1" ]; then
   echo "=> Building cenotelie/hime-build-env:latest"
