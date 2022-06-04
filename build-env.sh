272288ec088b5a6700c88547dbc10440181709df
Laurent Wouters <lwouters@cenotelie.fr>
2022-06-04 13:35:58 +0200

    Cleanup build-env

--- build-env.sh
+++ build-env.sh
@@ -13,23 +13,3 @@ if [[ ! -z "$GIT_TAG" ]]; then
 fi
 
 VERSION=$(grep "version" "$ROOT/runtime-rust/Cargo.toml" | grep -o -E "([[:digit:]]+\\.[[:digit:]]+\\.[[:digit:]])+")
-
-MONO=$(which mono)
-if [ -z "$MONO" ]
-then
-  echo "Mono is not installed!"
-  exit 1
-fi
-MONO=$(mono --version | grep 'version')
-
-MONO461=/usr/lib/mono/4.6.1-api
-if [ ! -f "$MONO461/mscorlib.dll" ]; then
-  echo "Required Mono assemblies for .Net Framework 4.6.1 not found!"
-  exit 1
-fi
-
-MONO20=/usr/lib/mono/2.0-api
-if [ ! -f "$MONO20/mscorlib.dll" ]; then
-  echo "Required Mono assemblies for .Net Framework 2.0 not found!"
-  exit 1
-fi
