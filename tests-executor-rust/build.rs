286f7073214aba58054b9e79005b2e3703a2d831
Laurent Wouters <lwouters@cenotelie.fr>
2023-03-30 09:51:58 +0200

    Fix lints in build.rs

--- tests-executor-rust/build.rs
+++ tests-executor-rust/build.rs
@@ -18,12 +18,12 @@
 use std::process::Command;
 
 fn main() {
-    if let Ok(output) = Command::new("git").args(&["rev-parse", "HEAD"]).output() {
+    if let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() {
         let value = String::from_utf8(output.stdout).unwrap();
         println!("cargo:rustc-env=GIT_HASH={}", value);
     }
     if let Ok(output) = Command::new("git")
-        .args(&["tag", "-l", "--points-at", "HEAD"])
+        .args(["tag", "-l", "--points-at", "HEAD"])
         .output()
     {
         let value = String::from_utf8(output.stdout).unwrap();
